cf2fba3e18db552528bc3c65795a0f8b21535cc1
Matheus <math.leal14@gmail.com>
2024-02-13 15:50:51 -0300

    finalizado

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,11 @@
-import {MoonIcon} from "@heroicons/react/24/solid"
 import ToggleTheme from "../ToggleTheme/ToggleTheme"
 
-const Header = () => {
+const Header = ({ user }) => {
   return (
-    <div className="p-5 sm:m-5 sm:rounded-xl bg-alura-200 dark:bg-dark-200 flex justify-between items-center">
-      <span className="text-gray-100">Olá, Usuário</span>
-      <h1 className="text-gray-100 text-2xl sm:text-4xl">Alura Newsletter</h1>
-      <ToggleTheme className="h-8 text-gray-100 hidden sm:block cursor-pointer"/>
+    <div className="p-5 w-full sm:m-5 sm:rounded-xl bg-alura-200 dark:bg-dark-200 flex justify-between items-center">
+      <span className="text-gray-100">Olá, { user || "Usuário" }</span>
+      <h1 className="text-gray-100">Alura Newsletter</h1>
+      <ToggleTheme />
     </div>
   )
 }
